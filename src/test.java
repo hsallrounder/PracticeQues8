3c2eaf02764345ce05bf489c3bd55dc8851189a1
Himanshu Sharma <hsallrounder@gmail.com>
2021-10-13 11:43:46 +0530

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,19 @@
+public interface test{
+    public int square(int x);
+}
+class arithmetic implements test{
+    public int square (int y){
+        System.out.println("inside arith");
+        return (y*y);
+    }
+}
+class ToTestInt {
+    public int ans (int a){
+        arithmetic d = new arithmetic ();
+        return d.square(a);
+    }
+    public static void main(String[] args) {
+        ToTestInt x = new ToTestInt();
+        System.out.println(x.ans(18));
+    }
+}
